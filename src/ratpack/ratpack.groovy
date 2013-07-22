d9bdf3fdfaba5caf01aabde305c6e48346530d36
Rob Fletcher <robert.w.fletcher@gmail.com>
2013-07-22 17:04:12 -0500

    static compile templates

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -1,12 +1,14 @@
-import org.ratpackframework.groovy.templating.TemplateRenderer
+import org.ratpackframework.groovy.templating.*
 import static org.ratpackframework.groovy.RatpackScript.ratpack
 
 ratpack {
+	modules {
+		get(TemplatingModule).staticallyCompile = true
+	}
 	handlers {
 		get {
 			get(TemplateRenderer).render "index.html", title: "Mid-Century Ipsum"
 		}
-
 		assets "public"
 	}
 }
