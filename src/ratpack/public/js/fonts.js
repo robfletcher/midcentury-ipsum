f95f294630f7adab83e9dbcb6ce9c36c2d73528e
Rob Fletcher <robert.w.fletcher@gmail.com>
2013-07-26 23:08:34 +0100

    getting there with styling

---
+++ src/ratpack/public/js/fonts.js
@@ -0,0 +1,12 @@
+WebFontConfig = {
+    google: { families: [ 'Playfair+Display:900,700italic:latin', 'PT+Mono::latin', 'Cardo::latin' ] }
+};
+(function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+})();
