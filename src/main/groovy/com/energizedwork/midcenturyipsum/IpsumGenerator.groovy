2e1cd5430d2e346a400d9c8bb679b46225cbff00
Rob Fletcher <robert.w.fletcher@gmail.com>
2013-07-29 23:00:34 +0100

    improve ipsum generator
    Based on oliverdodd/jlorem

--- src/main/groovy/com/energizedwork/midcenturyipsum/IpsumGenerator.groovy
+++ src/main/groovy/com/energizedwork/midcenturyipsum/IpsumGenerator.groovy
@@ -6,29 +6,73 @@ import groovy.transform.CompileStatic
 class IpsumGenerator {
 
 	public static final int DEFAULT_PARAGRAPHS = 4
-	public static final int SENTENCES_PER_PARAGRAPH = 7
-	public static final IntRange WORDS_PER_SENTENCE = 4..8
+	public static final IntRange SENTENCES_PER_PARAGRAPH = 2..8
+	public static final IntRange WORDS_PER_SENTENCE = 3..9
 
 	private static final List<String> WORD_LIST = IpsumGenerator.getResource("corpus.txt").readLines()
+	private static final List<String> PUNCTUATION = [".", ".", "?", "!"]
 
 	private final Random randomizer = new Random()
 
-	Collection<String> generateText(int numParas = DEFAULT_PARAGRAPHS) {
-		def paras = []
-		numParas.times {
-			def sentences = []
-			SENTENCES_PER_PARAGRAPH.times {
-				int numWords = randomInt(WORDS_PER_SENTENCE)
-				def words = []
-				while (words.size() < numWords) {
-					def word = WORD_LIST[randomInt(0..<WORD_LIST.size())]
-					if (!words.contains(word)) words << word.toLowerCase()
-				}
-				sentences << words.join(" ").capitalize()
-			}
-			paras << sentences.join(". ") + "."
+	final Collection<String> paragraphs(int count = DEFAULT_PARAGRAPHS) {
+		def paragraphs = []
+		count.times {
+			paragraphs << paragraph()
 		}
-		return paras
+		return paragraphs
+	}
+
+	final String paragraph() {
+		sentences randomInt(SENTENCES_PER_PARAGRAPH)
+	}
+
+	final String sentences(int count) {
+		def buffer = new StringBuilder()
+		count.times {
+			buffer << sentence() << " "
+		}
+		return buffer.toString().trim()
+	}
+
+	final String sentence() {
+		def word = randomWord().capitalize()
+		def buffer = new StringBuilder(word) << " "
+
+		if (randomizer.nextBoolean()) {
+			randomInt(1..2).times {
+				buffer << sentenceFragment() << ", "
+			}
+		}
+
+		buffer << sentenceFragment() << randomPunctuation()
+		return buffer as String
+	}
+
+	final String sentenceFragment() {
+		words randomInt(WORDS_PER_SENTENCE)
+	}
+
+	final String words(int count) {
+		def words = []
+		while (words.size() < count) {
+			def word = randomWord()
+			if (!words.contains(word)) {
+				words << word
+			}
+		}
+		return words.join(" ")
+	}
+
+	final String randomPunctuation() {
+		randomElement PUNCTUATION
+	}
+
+	final String randomWord() {
+		randomElement(WORD_LIST).toLowerCase()
+	}
+
+	private String randomElement(List<String> strings) {
+		strings[randomInt(0..<strings.size())]
 	}
 
 	private int randomInt(IntRange range) {
