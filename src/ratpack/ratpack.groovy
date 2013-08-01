b08882c15214f17ce24ab7912ba7a5bc0475def3
Luke Daley <ld@ldaley.com>
2013-08-01 08:28:58 +0100

    Use builder DSL sugar.

--- src/ratpack/ratpack.groovy
+++ src/ratpack/ratpack.groovy
@@ -4,6 +4,8 @@ import static groovy.json.JsonOutput.toJson
 import static org.ratpackframework.groovy.RatpackScript.ratpack
 import static com.energizedwork.midcenturyipsum.IpsumGenerator.DEFAULT_PARAGRAPHS
 
+import static org.ratpackframework.groovy.Util.with
+
 ratpack {
 	modules {
 		register new IpsumGeneratorModule()
@@ -15,16 +17,17 @@ ratpack {
 				int paras = pathTokens.paras?.toInteger() ?: DEFAULT_PARAGRAPHS
 				paras = Math.min(paras, 25)
 				def ipsum = generator.paragraphs(paras)
-				accepts.type("text/plain") {
-					response.send ipsum.join("\n")
+				with(accepts) {
+					type("text/plain") {
+						response.send ipsum.join("\n")
+					}
+					type("application/json") {
+						response.send toJson(ipsum)
+					}
+					type("text/html") {
+						renderer.render "index.html", ipsum: ipsum.collect { "<p>$it</p>" }.join(""), paras: paras
+					}
 				}
-				.type("application/json") {
-					response.send toJson(ipsum)
-				}
-				.type("text/html") {
-					renderer.render "index.html", ipsum: ipsum.collect { "<p>$it</p>" }.join(""), paras: paras
-				}
-				.build()
 			} catch (NumberFormatException e) {
 				clientError 400
 			}
