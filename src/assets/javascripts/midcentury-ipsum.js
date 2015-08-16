65c5da748c221e5fd1351a31473dd8912bb551e3
Rob Fletcher <rfletcher@netflix.com>
2015-08-15 17:39:38 -0700

    don't need config.rb any more

--- src/assets/javascripts/midcentury-ipsum.js
+++ src/assets/javascripts/midcentury-ipsum.js
@@ -1,24 +1,24 @@
 WebFontConfig = {
-    google: { families: [ 'Playfair+Display:900,700italic:latin', 'Cardo::latin' ] }
+  google: {families: ['Playfair+Display:900,700italic:latin', 'Cardo::latin']}
 };
-(function() {
-    var wf = document.createElement('script');
-    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
-        '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
-    wf.type = 'text/javascript';
-    wf.async = 'true';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(wf, s);
+(function () {
+  var wf = document.createElement('script');
+  wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+    '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+  wf.type = 'text/javascript';
+  wf.async = 'true';
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(wf, s);
 })();
 
-(function($) {
-    $('[name="paras"]').on('change', function(event) {
-        $.getJSON("/" + $(event.target).val(), function(json) {
-            var paragraphs = "";
-            $.each(json, function(index, item) {
-                paragraphs += "<p>" + item + "</p>";
-            });
-            $('#ipsum').html(paragraphs);
-        });
+(function ($) {
+  $('[name="paras"]').on('change', function (event) {
+    $.getJSON("/" + $(event.target).val(), function (json) {
+      var paragraphs = "";
+      $.each(json, function (index, item) {
+        paragraphs += "<p>" + item + "</p>";
+      });
+      $('#ipsum').html(paragraphs);
     });
+  });
 })(Zepto);
